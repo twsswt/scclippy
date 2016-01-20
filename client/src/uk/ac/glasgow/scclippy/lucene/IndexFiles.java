d287469dea713f26c460429552e5a1f85df58b48
thrios <thrios123@gmail.com>
2016-01-20 15:35:20 +0000

    Directory changes #30 Added server code

--- client/src/uk/ac/glasgow/scclippy/lucene/IndexFiles.java
+++ client/src/uk/ac/glasgow/scclippy/lucene/IndexFiles.java
@@ -27,9 +27,7 @@ import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import uk.ac.glasgow.scclippy.uicomponents.MainWindow;
 
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -37,7 +35,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Date;
 
 /**
  * Index all text files under a directory.
