c35ab81d4f27367079f856f20719ac2ecd703db3
thrios <thrios123@gmail.com>
2016-01-20 21:25:55 +0000

    #30 Server (finally)

---
+++ server/src/main/java/uk/ac/glasgow/scclippy/lucene/File.java
@@ -0,0 +1,37 @@
+package uk.ac.glasgow.scclippy.lucene;
+
+import java.nio.file.Paths;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+public class File {
+
+    private String filepath;
+    private String content;
+    final private String FILE_TYPE = ".txt";
+
+    File(String filepath, String content) {
+        this.filepath = filepath;
+        this.content = content;
+    }
+
+    public String getFilepath() {
+        return filepath;
+    }
+
+    public String getFileName() {
+        if (filepath == null) {
+            return null;
+        }
+        String filename = Paths.get(filepath).getFileName().toString();
+        String[] questionAndAnswer = filename.split("-");
+        String questionOrAnswer = questionAndAnswer[questionAndAnswer.length - 1];
+
+        return questionOrAnswer.substring(0, questionOrAnswer.length() - FILE_TYPE.length());
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
