98df0276262ff65856a77a1ae7ecef44e2aec9ff
thrios <thrios123@gmail.com>
2016-02-17 13:34:54 +0000

    Added first test case

---
+++ client/tests/uk/ac/glasgow/scclippy/plugin/search/StackExchangeSearchTest.java
@@ -0,0 +1,14 @@
+package uk.ac.glasgow.scclippy.plugin.search;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StackExchangeSearchTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
