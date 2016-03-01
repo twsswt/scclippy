c68b092f452c8d96cce29557b6a76fafe58a11e3
twsswt <timothy.storer@glasgow.ac.uk>
2016-03-01 10:28:50 +0000

    Added changes to database indexing to branch.

---
+++ server/src/main/java/uk/ac/glasgow/scclippy/lucene/LuceneFacade.java
@@ -0,0 +1,166 @@
+package uk.ac.glasgow.scclippy.lucene;
+/*
+
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *	 http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import static java.lang.String.format;
+import static java.util.Arrays.asList;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.util.CharArraySet;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.LongField;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+/**
+ * Utility class for indexing the Stackoverflow Postgres Database.s
+ */
+public class LuceneFacade {
+
+	private int documentsIndexed;
+	
+	private final Connection connection;
+	
+	private final Path indexDirectoryPath;
+	
+	public LuceneFacade (Connection connection, Path indexDirectoryPath) {
+		this.connection = connection;		
+		this.indexDirectoryPath = indexDirectoryPath;
+		
+		if (!indexIsReady()){
+			new Thread (){
+				public void run (){
+					try {
+						indexDocuments();
+					} catch (IOException | SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}.start();
+		}
+			
+		
+	}	
+
+	private void indexDocuments() throws IOException, SQLException {
+						
+		IndexWriter indexWriter = createIndexWriter();
+		
+		Statement statement = connection.createStatement();
+		
+		ResultSet resultSet = statement.executeQuery("SELECT * FROM posts");
+		
+		while (resultSet.next()){
+						
+			Document document = createDocument(resultSet);
+			documentsIndexed ++;
+			indexWriter.addDocument(document);		
+			
+		}
+	}
+
+	private boolean indexIsReady() {
+		File indexDirectory = indexDirectoryPath.toFile();
+
+		return 	indexDirectory.exists() && 
+				indexDirectory.isDirectory() &&
+				indexDirectory.listFiles().length > 0 &&
+				!asList(indexDirectory.list()).contains("write.lock");
+				
+	}
+	
+	private IndexWriter createIndexWriter() throws IOException {
+		Directory indexDirectory = FSDirectory.open(indexDirectoryPath);
+		Analyzer analyzer = new StandardAnalyzer(CharArraySet.EMPTY_SET);
+		IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
+		indexWriterConfig.setOpenMode(OpenMode.CREATE);		
+		IndexWriter indexWriter = new IndexWriter(indexDirectory, indexWriterConfig);
+		return indexWriter;
+	}
+
+	private Document createDocument(ResultSet resultSet) throws SQLException {
+		
+		String body = resultSet.getString("Body");
+		String postId = resultSet.getString("Id");
+		Long lastEditDate = resultSet.getLong("LastEditDate");
+		
+		Document document = new Document();
+		Field pathField = new StringField("Id", postId, Field.Store.YES);
+		document.add(pathField);
+		document.add(new LongField("LastEditDate", lastEditDate, Field.Store.NO));
+		document.add(new TextField("Body", new StringReader(body)));
+		return document;
+	}
+
+	public int getNumberofRecordsIndexed() {
+		return documentsIndexed;
+	}
+	
+	public TopDocs searchDocuments (String queryString, Integer desiredHits) throws IOException, ParseException {
+		
+		if (!indexIsReady())
+			throw new IOException (
+				format(
+					"The index at path [%s] is not currently available.  Perhaps it is being prepared for use?.",
+					indexDirectoryPath));
+		
+		IndexSearcher searcher = getSearcher();
+
+		Analyzer analyzer = new StandardAnalyzer(CharArraySet.EMPTY_SET);
+		QueryParser parser = new QueryParser("Body", analyzer);
+		String line = queryString.trim();
+		Query query = parser.parse(QueryParser.escape(line));
+
+		return searcher.search(query, desiredHits);
+
+	}
+	
+	public Document getDocument(int documentIndex) throws IOException{
+		return getSearcher().doc(documentIndex);
+	}
+
+	private IndexSearcher getSearcher()	throws IOException {
+		IndexReader reader = DirectoryReader.open(FSDirectory.open(indexDirectoryPath));
+		IndexSearcher searcher = new IndexSearcher(reader);
+		return searcher;
+	}
+	
+}
--- server/src/main/java/uk/ac/glasgow/scclippy/rest/SearchService.java
+++ server/src/main/java/uk/ac/glasgow/scclippy/rest/SearchService.java
@@ -1,5 +1,11 @@
 package uk.ac.glasgow.scclippy.rest;
  
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,45 +15,88 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import uk.ac.glasgow.scclippy.plugin.lucene.File;
-import uk.ac.glasgow.scclippy.plugin.lucene.SearchFiles;
+import uk.ac.glasgow.scclippy.lucene.LuceneFacade;
  
 @Path("/search")
 public class SearchService {
  
-	private static String indexPath = "D:/finalIndex"; //TODO
+	private static String indexPathString = "./target/lucene-index"; //TODO - should be configurable in the WAR.
 	
+	private static final String dbURL = "jdbc:postgresql://localhost:5432/stackoverflow";
+	private static final String dbUsername = "stackoverflow";
+	private static final String dbPassword = "everstepf00t";
+	
+	private LuceneFacade luceneFacade;
+	
+	public SearchService (){
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+			luceneFacade = new LuceneFacade(connection, Paths.get(indexPathString).toAbsolutePath());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+
+	}
+	
+	@SuppressWarnings("unchecked")
 	@GET
 	@Path("/{query}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response indexSearch(
 			@PathParam("query") String query, 
-			@DefaultValue("contents") @QueryParam("field") String field, 
+			@DefaultValue("Body") @QueryParam("field") String field, 
 			@DefaultValue("5") @QueryParam("posts") Integer postCount) {
 		
-		File[] output = null;
+		if (luceneFacade == null)
+			return Response.status(200).entity("Couldn't connect to search index.").build();
+
+		TopDocs topDocs;
 		try {
-			output = SearchFiles.search(indexPath, field, query, postCount);
-		} catch (Exception e) {
-			e.printStackTrace();
+			topDocs = luceneFacade.searchDocuments(query, postCount);
+		} catch (IOException e) {
+			return Response.status(200).entity("Search index is not currently available.").build();
+		} catch (ParseException e) {
+			return Response.status(200).entity("Invalid query.").build();
+		}			
+		
+		JSONArray records = new JSONArray();
+		
+		for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+			JSONObject record = new JSONObject();
+			
+			Document document;
+			try {
+				document = luceneFacade.getDocument(scoreDoc.doc);
+				
+				record.put("id", document.getField("Id"));
+				record.put("content", document.getField("Content"));
+				record.put("score", scoreDoc.score);
+				
+				records.add(record);
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
 		}
 		
-		if (output == null)
-			return Response.status(200).entity("").build();
-		
-		JSONArray arr = new JSONArray();
-		for (int i = 0; i < output.length; i++) {
-			JSONObject obj = new JSONObject();
-			obj.put("id", output[i].getFileName());
-			obj.put("content", output[i].getContent());
-			obj.put("score", output[i].getScore());
-			arr.add(obj);
-		}
 		JSONObject finalObject = new JSONObject();
-		finalObject.put("results", arr);
+		finalObject.put("results", records);
 		
 		return Response.status(200).entity(finalObject.toJSONString()).build();
 	}
